6366d9b28b22a69377b16969ae518b857ad9233e
Suhas Dissanayake <suhasdissa@gmail.com>
2025-12-10 18:05:38 +0530

    feat: add light mode

--- resources/views/components/seds-logo.blade.php
+++ resources/views/components/seds-logo.blade.php
@@ -1,7 +1,7 @@
 @props(['class' => ''])
 
 <div {{ $attributes->merge(['class' => 'flex items-center gap-3 ' . $class]) }}>
-    <x-app-logo-icon class="w-24 h-24 text-white" />
+    <x-app-logo-icon class="w-24 h-24 text-neutral dark:text-white" />
     <div class="flex flex-col">
         <span class="text-xl font-bold text-base-content">SEDS Mora</span>
         <span class="text-xs text-base-content/70">Space Exploration & Development</span>
